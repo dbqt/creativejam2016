2062ef64b316cd91097637c7ec2ab4945e177e36
Catalin <hugo_bedard@hotmail.com>
2016-08-20 10:13:13 -0400

    Script now generates a marshmallow for show that has a range-driven colorIndex

--- equipe3officiellement/Assets/Scripts/MarsmallowBehavior.cs
+++ equipe3officiellement/Assets/Scripts/MarsmallowBehavior.cs
@@ -10,15 +10,20 @@ public class MarsmallowBehavior : MonoBehaviour {
     public Color black;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+        Debug.Log("Start");
         material = gameObject.transform.FindChild("Capsule").GetComponent<MeshRenderer>().material;
+        if (material == null)
+        {
+            Debug.Log("Heeelp!");
+        }
         colorIndex = 0;
         material.SetColor("_Color", white);
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         if (Input.GetKeyDown(KeyCode.X))
         {
             colorIndex = Mathf.Clamp(colorIndex += 2.0f, 0.0f, 100.0f);
@@ -29,17 +34,30 @@ public class MarsmallowBehavior : MonoBehaviour {
             colorIndex = Mathf.Clamp(colorIndex -= 2.0f, 0.0f, 100.0f);
         }
 
-        if (colorIndex >0  && colorIndex <=75)
-        {
-            material.SetColor("_Color",Color.Lerp(white, orange, colorIndex / 75));
-            
-        }
-        else if (colorIndex > 75 && colorIndex < 100)
-        {
-            material.SetColor("_Color", Color.Lerp(orange, black, (colorIndex-75) / 25));
-        }
-
-
-        Debug.Log(colorIndex);
+        //applyColorIndex(colorIndex);
+        //Debug.Log(colorIndex);
 	}
+
+    public void applyColorIndex(float newColorIndex)
+    {
+        Debug.Log("Apply col ind");
+        if (material != null)
+        {
+            if (newColorIndex > 0 && newColorIndex <= 75)
+            {
+                material.SetColor("_Color", Color.Lerp(white, orange, newColorIndex / 75));
+
+            }
+            else if (newColorIndex > 75 && newColorIndex < 100)
+            {
+                material.SetColor("_Color", Color.Lerp(orange, black, (newColorIndex - 75) / 25));
+            }
+        }
+        else
+        {
+            Debug.Log("Error! Material does not exist!");
+        }
+    }
+
 }
+
