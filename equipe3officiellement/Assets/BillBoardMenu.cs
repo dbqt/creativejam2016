9fa6a5c4cc8e2b683b478c684f33a1de8bd0050e
Pierre-Olivier Chartrand <pierreolivier.chartrand@gmail.com>
2016-08-21 09:50:19 -0400

    no message

---
+++ equipe3officiellement/Assets/BillBoardMenu.cs
@@ -0,0 +1,71 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class BillBoardMenu : MonoBehaviour
+{
+    public TextMesh start;
+    public TextMesh quit;
+    public Animation buttonStart;
+    public Animation buttonQuit;
+    private int selected = 0;
+    private float lockTime = 0.01f;
+    // Use this for initialization
+    void Start()
+    {
+        selectItem();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float axs = Input.GetAxis("Vertical");
+        if (axs != 0 && Time.time >= lockTime)
+        {
+            lockTime = Time.time + 0.5f;
+            if (axs > 0)
+            {
+                selected += 1;
+                selected %= 2;
+            }
+            else if (axs < 0)
+            {
+                selected -= 1;
+                selected = Mathf.Abs(selected);
+            }
+            selectItem();
+        }
+        if (Input.GetButtonDown("Submit"))
+        {
+            if (selected == 0)
+            {
+                GetComponent<Animation>().Play("ButtonClickStart");
+                Camera.main.GetComponent<Animation>().Play("MainCameraMenuGame");
+            }
+            else
+            {
+                GetComponent<Animation>().Play("ButtonClickQuit");
+                StartCoroutine(QuitEnu());
+            }
+
+        }
+    }
+    IEnumerator QuitEnu()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Application.Quit();
+    }
+    void selectItem()
+    {
+        if (selected == 0)
+        {
+            start.color = Color.white;
+            quit.color = new Color(140 / 255f, 140 / 255f, 140 / 255f);
+        }
+        else
+        {
+            quit.color = Color.white;
+            start.color = new Color(140 / 255f, 140 / 255f, 140 / 255f);
+        }
+    }
+}
---
+++ equipe3officiellement/Assets/CameraMenuMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraMenuMovement : MonoBehaviour {
+
+    public Image fadePanel;
+
+    void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void startFadeOut()
+    {
+        StartCoroutine(FadeOut());
+    }
+    public IEnumerator FadeOut()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+
+            Color col = fadePanel.color;
+            col.a += 0.01f;
+            fadePanel.color = col;
+            yield return new WaitForSeconds(0.01f);
+        }
+        GameController.instance.StartGame();
+        
+    }
+    public IEnumerator FadeIn()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+
+            Color col = fadePanel.color;
+            col.a -= 0.10f;
+            fadePanel.color = col;
+            yield return new WaitForSeconds(0.1f);
+        }
+        
+    }
+}
