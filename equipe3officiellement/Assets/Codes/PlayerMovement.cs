7a80eee73a25eb9b4a91c40a5becc0d093018244
Catalin <jeremiecoul@hotmail.com>
2016-08-21 13:34:12 -0400

    finished sound settings

--- equipe3officiellement/Assets/Codes/PlayerMovement.cs
+++ equipe3officiellement/Assets/Codes/PlayerMovement.cs
@@ -11,15 +11,49 @@ public class PlayerMovement : MonoBehaviour {
     public float MaxSpeed;
     public LayerMask GroundMask;
 
+    public AudioClip[] audioClips;
+    public AudioClip[] collisionClips;
+    public float soundPlayRate;
+
+    private float nextSoundPlay = 0.0f;
+    private bool needsReplacement = true;
+
+    private bool needsCollSoundReplacement = true;
+
     private Vector3 projectedPropulsor1;
     private Vector3 projectedPropulsor2;
+
+    private AudioSource audioSource;
 
     // Use this for initialization
     void Start () {
         projectedPropulsor1 = Vector3.zero;
         projectedPropulsor2 = Vector3.zero;
+
+        audioSource = gameObject.GetComponent<AudioSource>();
+        SoundReplacer();
+
     }
 	
+    void SoundReplacer()
+    {
+        if (needsReplacement == true && audioSource.isPlaying == false)
+        {
+            audioSource.clip = audioClips[Mathf.FloorToInt(Random.Range(0, 3))];
+            needsReplacement = false;
+        }
+    }
+
+    void CollisionSoundReplacer()
+    {
+        if (needsCollSoundReplacement == true && audioSource.isPlaying == false)
+        {
+            audioSource.clip = collisionClips[Mathf.FloorToInt(Random.Range(0, 4))];
+            needsCollSoundReplacement = false;
+        }
+    }
+
+
 	void FixedUpdate () {
         // add translation movement
         float angle1 = Propulsor1.eulerAngles.y;
@@ -38,8 +72,37 @@ public class PlayerMovement : MonoBehaviour {
         transform.GetChild(transform.childCount - 1).LookAt(transform.position + direction.normalized);
     }
 
+    void OnCollisionEnter (Collision other)
+    {
+        Debug.Log("Collision! audioSource.clip = " + audioSource.clip);
+        if (other.gameObject.GetComponent<PlayerMovement>() != null && Time.time > nextSoundPlay && audioSource.clip != null)
+        {
+            nextSoundPlay = Time.time + soundPlayRate;
+            needsCollSoundReplacement = true;
+            audioSource.Play();
+        }
+    }
     void Update ()
     {
+        
+        if (Input.anyKeyDown && Time.time > nextSoundPlay && audioSource.clip != null)
+        {
+            nextSoundPlay = Time.time + soundPlayRate;
+            //audioSource.Play();
+            needsReplacement = true;
+        }
+        
+        if (needsReplacement && Time.time > nextSoundPlay)
+        {
+            SoundReplacer();
+        }
+
+        if (needsCollSoundReplacement && Time.time > nextSoundPlay)
+        {
+            CollisionSoundReplacer();
+        }
+
+
         Quaternion proAngle = Quaternion.identity;
 
         proAngle.eulerAngles = new Vector3(0f, -Propulsor1.eulerAngles.y, 0f);
@@ -67,4 +130,11 @@ public class PlayerMovement : MonoBehaviour {
             o.gameObject.GetComponent<GroundController>().node.flameRate -= FireExtinguishingPower * PropulsorRange / Vector3.Distance(o.transform.position, transform.position) * Time.deltaTime;
         }
     }
+
+    void playSound()
+    {
+
+    }
+
+
 }
