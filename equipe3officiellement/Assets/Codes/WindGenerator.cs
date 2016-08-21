01fa717048dcafe177d612ddb56dc6bcc132ae74
Pierre Hayeur <ld.pierre.hayeur@gmail.com>
2016-08-21 14:51:52 -0400

    audio source sur t_move et Menu

---
+++ equipe3officiellement/Assets/Codes/WindGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindGenerator : MonoBehaviour {
+
+	public AudioSource windAudio;
+	private float nextSnd = 0.0f;
+	public float sndDelay;
+
+	// Use this for initialization
+	void Start () {
+		windAudio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextSnd) {
+			nextSnd = Time.time + sndDelay;
+			windAudio.Play ();
+		}
+	}
+}
