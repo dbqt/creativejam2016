a8458879bee28594f93da757a15ae78a69145297
Pierre-Olivier Chartrand <pierreolivier.chartrand@gmail.com>
2016-08-21 04:25:06 -0400

    no message

---
+++ equipe3officiellement/Assets/Draw3Controller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Draw3Controller : MonoBehaviour {
+
+
+    public Image img1;
+    public Image img2;
+    public Image img3;
+    public Text txt1;
+    public Text txt2;
+    public Text txt3;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
